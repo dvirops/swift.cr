4f3497e26194d5fd9860c027dee7a60e2e2d9c62
Josh Dvir <josh@dvir.us>
2019-04-09 21:18:56 -0400

    a lot more docs

--- src/swift/client/release.cr
+++ src/swift/client/release.cr
@@ -6,9 +6,13 @@ module Swift
     module Release
       # Retrieves a list of all releases.
       #
-      # - option params [String] :namespace [name of namespace] | EMPTY (for all namespaces)
-      # - option params [String] :sort_by UNKNOWN | NAME | LAST_RELEASED
       # - option params [Bool] :all true | false
+      # - option params [String] :filter A regular expression used to filter which releases should be listed. Anything that matches the regexp will be included in the results.
+      # - option params [Int64] :limit The maximum number of releases to be returned
+      # - option params [String] :namespace [name of namespace] | EMPTY (for all namespaces)
+      # - option params [String] :offset Offset is the last release name that was seen. The next listing operation will start with the name after this one. Example: If list one returns albert, bernie, carl, and sets 'next: dennis'. dennis is the offset. Supplying 'dennis' for the next request should cause the next batch to return a set of results starting with 'dennis'.
+      # - option params [String] :sort_by UNKNOWN | NAME | LAST_RELEASED
+
       # - option params [String] :sort_order ASC | DESC
       # - option params [String] :status_codes UNKNOWN | DEPLOYED | DELETED | SUPERSEDED | FAILED | DELETING
       # - return [JSON::Any] List of all releases.
@@ -25,13 +29,14 @@ module Swift
       # Retrieves status information for the specified release.
       #
       # - release [String] The name of the release.
+      # - option params [String] :version Version of the release.
       # - return [JSON::Any] Status information for the specified release.
       #
       # ```
       # client.release_status("release_x")
       # ```
-      def release_status(release : String) : JSON::Any
-        get("/tiller/v2/releases/#{release}/status/json").parse
+      def release_status(release : String, params : (Hash(String, _) | NamedTuple)? = nil) : JSON::Any
+        get("/tiller/v2/releases/#{release}/status/json", params: params).parse
       end
 
       # Retrieves the release content (chart + value) for the specified release.
@@ -64,13 +69,13 @@ module Swift
       # Rolls back a release to a previous version
       #
       # - release [String] The name of the release.
-      # - option params [Bool] :dry_run true | false
       # - option params [Bool] :disable_hooks true | false
-      # - option params [Int32] :version the version of the release to deploy.
-      # - option params [Bool] :recreate true | false
-      # - option params [Bool] :wait true | false
+      # - option params [Bool] :dry_run true | false
       # - option params [Bool] :force true | false
+      # - option params [Bool] :recreate true | false
       # - option params [Int64] :timeout true | false
+      # - option params [Int32] :version the version of the release to deploy.
+      # - option params [Bool] :wait true | false
       # - return [JSON::Any] Release content (chart + value) for the specified release.
       #
       # ```
@@ -85,7 +90,22 @@ module Swift
       # Requests installation of a chart as a new release.
       #
       # - release [String] The name of the release.
-      # - json [JSON] JSON Body to send to Tiller. example - [link placeholder]
+      # - json [String] :ca_bundle PEM encoded CA bundle used to sign server certificate of chart repository.
+      # - json [String] :chart A helm package that contains metadata, a default config, zero or more. optionally parameterizable templates, and zero or more charts (dependencies).
+      # - json [String] :chart_url URL to download chart archive.
+      # - json [String] :client_certificate PEM-encoded data passed as a client cert to chart repository.
+      # - json [String] :client_key PEM-encoded data passed as a client key to chart repository.
+      # - json [Bool] :disable_hooks Causes the server to skip running any hooks for the install.
+      # - json [Bool] :dry_run if true, will run through the release logic, but neither create a release object nor deploy to Kubernetes. The release object returned in the response will be fake.
+      # - json [Bool] :insecure_skip_verify Skip certificate verification for chart repository.
+      # - json [String] :namespace The kubernetes namespace of the release.
+      # - json [String] :password The password for basic authentication to the chart repository.
+      # - json [Bool] :reuse_name Requests that Tiller re-uses a name, instead of erroring out.
+      # - json [Int64] :timeout Specifies the max amount of time any kubernetes client command can run.
+      # - json [String] :token The bearer token for authentication to the chart repository.
+      # - json [String] :username The username for basic authentication to the chart repository.
+      # - json [Hash] :values Supplies values to the parametrizable templates of a chart.
+      # - json [Bool] :wait if true, will wait until all Pods, PVCs, and Services are in a ready state before marking the release as successful. It will wait for as long as timeout.
       # - return [JSON::Any] Release content (chart + value) for the specified release.
       #
       # ```
@@ -101,6 +121,22 @@ module Swift
       # Updates release content.
       #
       # - release [String] The name of the release.
+      # - json [String] :ca_bundle PEM encoded CA bundle used to sign server certificate of chart repository.
+      # - json [String] :chart A helm package that contains metadata, a default config, zero or more. optionally parameterizable templates, and zero or more charts (dependencies).
+      # - json [String] :chart_url URL to download chart archive.
+      # - json [String] :client_certificate PEM-encoded data passed as a client cert to chart repository.
+      # - json [String] :client_key PEM-encoded data passed as a client key to chart repository.
+      # - json [Bool] :disable_hooks Causes the server to skip running any hooks for the upgrade.
+      # - json [Bool] :dry_run if true, will run through the release logic, but neither create a release object nor deploy to Kubernetes. The release object returned in the response will be fake.
+      # - json [Bool] :insecure_skip_verify Skip certificate verification for chart repository.
+      # - json [String] :namespace The kubernetes namespace of the release.
+      # - json [String] :password The password for basic authentication to the chart repository.
+      # - json [Bool] :reuse_name Requests that Tiller re-uses a name, instead of erroring out.
+      # - json [Int64] :timeout Specifies the max amount of time any kubernetes client command can run.
+      # - json [String] :token The bearer token for authentication to the chart repository.
+      # - json [String] :username The username for basic authentication to the chart repository.
+      # - json [Hash] :values Supplies values to the parametrizable templates of a chart.
+      # - json [Bool] :wait if true, will wait until all Pods, PVCs, and Services are in a ready state before marking the release as successful. It will wait for as long as timeout.
       # - json [JSON] JSON Body to send to Tiller.
       # - return [JSON::Any] Release content (chart + value) for the specified release.
       #
@@ -117,14 +153,16 @@ module Swift
       # Requests deletion of a named release
       #
       # - release [String] The name of the release.
-      # - option purge [Bool] Removes the release from the store and make its name free for later use
+      # - option params [Bool] :disable_hooks causes the server to skip running any hooks for the uninstall.
+      # - option params [Bool] :purge Removes the release from the store and make its name free for later use.
+      # - option params [Int64] :timeout Specifies the max amount of time any kubernetes client command can run.
       # - return [JSON::Any] Release content (chart + value) for the specified release.
       #
       # ```
       # client.uninstall_release("release_x", true)
       # ```
-      def uninstall_release(release : String, purge : Bool? = nil) : JSON::Any
-        delete("/tiller/v2/releases/#{release}/json?purge=#{purge}").parse
+      def uninstall_release(release : String, params : (Hash(String, _) | NamedTuple)? = nil) : JSON::Any
+        delete("/tiller/v2/releases/#{release}/json", params: params).parse
       end
     end
   end
